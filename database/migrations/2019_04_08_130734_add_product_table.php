bd55374ce5c935901e511e7a848d2a5823672ecf
Nguyen Nguyen Huy <kentomskill@gmail.com>
2019-04-08 20:47:38 +0700

    Init Migration

---
+++ database/migrations/2019_04_08_130734_add_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name')->nullable();
+            $table->decimal('price', 24, 0)->default('0');
+            $table->boolean('highlight');
+            $table->unsignedInteger('quantity');
+            $table->string('avatar')->nullable();
+            $table->string('description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2019_04_08_133047_add_product_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProductCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->bigInteger('category_id')->unsigned()->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category');
+    }
+}
